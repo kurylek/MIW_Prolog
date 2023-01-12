72b2ab9e0ba37d758aae7308cb9b1519e890e5b0
Kamil Kuryłek <kurylek98@gmail.com>
2023-01-12 21:42:49 +0100

    Fix directions in descriptions; add exit in elevator

--- MIW_Prolog.pl
+++ MIW_Prolog.pl
@@ -2,19 +2,19 @@
 :- dynamic position/2.
 
 % Create rooms- room(roomName, Description)
-room(corridor, 'Corridor again.. Ugh.. Let`s enter apartment.. The door is in the west!').
+room(corridor, 'Corridor again.. Ugh.. Let`s enter apartment.. The door is in front of you!').
 room(hall, 'All roads lead to Ro.. Hall of course! I think there sould be something in storage.
-            Or maybe.. Let`s move! Bathroom should be on north, kitchen on south..
-            Bedroom in on west and to go out head to corridor on east!').
+            Or maybe.. Let`s move! Bathroom should be on right, kitchen on left..
+            Bedroom in front and to go out turn around to corridor behind you!').
 room(bathroom, 'Thank God it`s clean right here! I think it`s not time to take a bath or to use wc!
-                Let`s go back to hall- it`s on south!').
+                Let`s go back to hall- it`s on left!').
 room(bedroom, 'Look at this bed! It`s so huge.. How about a quick nap? [..] No?
-               So let`s check this big wardrobe or let`s go back to hall- it`s on east!').
+               So let`s check this big wardrobe or let`s go back to hall- it`s behind you!').
 room(kitchen, 'Wow.. I wish I know how to cook, that microwave was worth it`s price..
-               You can go to living room on east, or to hall on north.').
-room(livingroom, 'Living room without TV would be deadroom! You can go out to the balcony on west,
-                  or to kitchen on west. It`s your choice!').
-room(balcony, 'Maybe is small, but hey! It`s yours! Take a look around, or go back inside to the east.').
+               You can go to living room it`s ahead, or to hall on right.').
+room(livingroom, 'Living room without TV would be deadroom! You can go out to the balcony- it`s in front,
+                  or go back to kitchen. It`s your choice!').
+room(balcony, 'Maybe is small, but hey! It`s yours! Take a look around, or go back inside.').
 
 % Create objects- object(objectname)
 object(elevator).
@@ -56,6 +56,11 @@ get_to(livingroom, kitchen, back).
 get_to(livingroom, balcony, forward).
 get_to(balcony, livingroom, back).
 
+%  Go to elevator
+move(elevator) :-
+    position(you, Position),
+    elevator(Position).
+
 % Go to given direction
 move(Direction) :- % goes in specific direction
     position(you, Position),
@@ -69,8 +74,21 @@ move(Direction) :- % goes in specific direction
 
 % Called when u can't go to given direction/typed something that you can't do
 move(_) :-
-    writeln('You can`t go there, or you are trying to do somthing stupid!'),
+    writeln('You can`t go there, or you are trying to do something stupid!'),
     lookAround.
+
+% Called when you go inside elevator
+elevator(Position) :-
+    position(you, Position),
+	is_in_room(elevator, Position),
+    writeln('Okey, let`s go out!'),
+    assert(position(you, out)),
+    !.
+
+% Called when there is no elevator in room
+elevator(_) :-
+    writeln('Are you blind? There is no elevator in here!'),
+    !.
 
 % Describe current room
 lookAround :-
@@ -78,6 +96,10 @@ lookAround :-
     room(Position, RoomDescription),
     writeln(RoomDescription).
 
+moveHandler :- % end of main loop
+    position(you, out),
+    writeln('You entered elevator and then leaved building!'),
+    !.
 % Loop with player moves
 moveHandler :-
     nl,
